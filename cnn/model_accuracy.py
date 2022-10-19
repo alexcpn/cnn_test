ac22230af8586f0469121c91376ebe926a2eb67c
Alex Punnen <alexcpn@gmail.com>
2022-10-19 19:39:27 +0530

    updated wrong class info

--- cnn/model_accuracy.py
+++ cnn/model_accuracy.py
@@ -1,5 +1,6 @@
 """
 Utility to check Precision and Recall of a  trained model
+Author - Alex Punnen 
 """
 
 from datetime import datetime
@@ -16,6 +17,8 @@ from collections import defaultdict
 from sklearn.metrics import classification_report
 import math
 import numpy as np
+from collections import Counter
+
 
 log.basicConfig(format="%(asctime)s %(message)s", level=log.INFO)
 
@@ -140,7 +143,8 @@ test_loader = torch.utils.data.DataLoader(
 if torch.cuda.is_available():
     model.to("cuda")
 
-md = defaultdict(list)
+precision_per_class = defaultdict(list)
+wrong_per_class = defaultdict(list)
 
 # In test phase, we don't need to compute gradients (for memory efficiency)
 with torch.no_grad():
@@ -156,11 +160,22 @@ with torch.no_grad():
         #print("predicted=",predicted.shape,predicted[10]) # predicted= torch.Size([64])
         #print("labels=",labels.shape,labels[10]) #labels= torch.Size([64]) 
         total += labels.size(0)
-        correct += (predicted == labels).float().sum().item() # this equals is a tensor type comparison
+        correct += (predicted == labels).float().sum().item()  #this is Torch Tensor semantics
         #print("correct",correct) # say 56 out of 64
         #print("classification_report",classification_report(labels.cpu(), predicted.cpu()))
+        #-------- Lets check also which classes are wrongly predicted with other classes (we need to clip at max prob > .5 to do)
+        mask=(predicted != labels)
+        wrong_predicted =torch.masked_select(predicted,mask)
+        wrong_labels =torch.masked_select(labels,mask)
+        zipped = zip(wrong_labels,wrong_predicted)
+
+        for _,j in enumerate(zipped):
+            wrong_per_class[j[0].item()].append(j[1].item())
+            #print(f"wrong_per_class{j[0].item()}={j[1].item()}",)
+
         for index, element in enumerate(categories):
-            cal = ((predicted == labels)*(labels ==index)).sum().item()/ ((labels == index).sum()) #this is Torch semantics
+            cal = ((predicted == labels)*(labels ==index)).sum().item()/ ((labels == index).sum()) #this is Torch Tensor semantics
+            wrong_class = (predicted != labels)*(labels == index)
             # >>> import torch
             # >>> some_integers = torch.tensor((2, 3, 5, 7, 11, 13, 17, 19))
             # >>> some_integers3 = torch.tensor((12, 3, 5, 7, 11, 13, 17, 19))
@@ -169,17 +184,21 @@ with torch.no_grad():
             # >>> ((some_integers ==some_integers3)*(some_integers >12)).sum().item()
             # 3
             if not math.isnan(cal):
-                md[element].append(cal.item())
+                precision_per_class[element].append(cal.item())
             #print(f"{element}={cal}")
-
-        #break
+        
     avg_accuracy =[]    
-    for key,val in md.items():
+    for key,val in precision_per_class.items():
         avg = np.mean(val)
-        md[key] = avg
+        precision_per_class[key] = avg
         avg_accuracy.append(avg)
         print(f"Accuracy of Class {key}={avg}")
+
+    # Just to cross check with the average accuracy results bleow    
     print(f"Average accuracy={np.mean(avg_accuracy)}")
+
+    for key,val in wrong_per_class.items():
+        print(f"wrong_per_class {categories[key]}={Counter(val)}")
 
     print(
         "Accuracy of the network on the {} test/validation images: {} %".format(
@@ -205,9 +224,8 @@ with torch.no_grad():
     # )
 
 """
-Output will be similar to
-
-2022-10-19 16:06:28,619 Gpu device NVIDIA GeForce RTX 3060 Laptop GPU
+Output 
+-----------------------------------------------------------------------------------------
 Accuracy of Class tench=0.8504464285714286
 Accuracy of Class English springer=0.6907253691128322
 Accuracy of Class cassette player=0.7420465648174286
@@ -219,5 +237,16 @@ Accuracy of Class gas pump=0.5343750034059797
 Accuracy of Class golf ball=0.5873511944498334
 Accuracy of Class parachute=0.5481353274413517
 Average accuracy=0.6384671883923666
+wrong_per_class tench=Counter({3: 25, 8: 16, 1: 10, 2: 7, 6: 3, 5: 3, 7: 2, 9: 1})
+wrong_per_class English springer=Counter({3: 39, 0: 23, 8: 21, 6: 7, 5: 7, 7: 3, 9: 3, 4: 3, 2: 3})
+wrong_per_class cassette player=Counter({7: 36, 6: 14, 3: 13, 8: 11, 0: 8, 5: 4, 1: 4, 4: 2})
+wrong_per_class chain saw=Counter({0: 49, 1: 30, 6: 27, 7: 22, 5: 21, 4: 19, 2: 12, 8: 8, 9: 4})
+wrong_per_class church=Counter({6: 23, 5: 21, 3: 20, 7: 19, 8: 16, 0: 14, 2: 10, 9: 7, 1: 5})
+wrong_per_class French horn=Counter({3: 64, 4: 26, 2: 22, 1: 21, 7: 19, 0: 13, 8: 12, 6: 11})
+wrong_per_class garbage truck=Counter({3: 28, 4: 23, 2: 14, 7: 14, 0: 8, 5: 5, 1: 4, 8: 2})
+wrong_per_class gas pump=Counter({2: 50, 6: 46, 3: 41, 4: 23, 1: 11, 5: 9, 8: 8, 0: 7, 9: 2})
+wrong_per_class golf ball=Counter({1: 38, 0: 37, 3: 27, 4: 17, 9: 11, 5: 10, 2: 9, 6: 7, 7: 6})
+wrong_per_class parachute=Counter({8: 56, 3: 46, 4: 19, 6: 13, 7: 12, 0: 10, 2: 6, 1: 6, 5: 2})
 Accuracy of the network on the 3925 test/validation images: 64.07643312101911 %
+-----------------------------------------------------------------------------------------
 """
