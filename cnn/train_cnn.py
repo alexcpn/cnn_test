ca9567f6d829b27e6199a6aec76938e17cf6563b
Alex Punnen <alexcpn@gmail.com>
2022-11-11 11:52:06 +0530

    formatted document

--- cnn/train_cnn.py
+++ cnn/train_cnn.py
@@ -6,18 +6,21 @@ https://github.com/fastai/imagenette
 Download the Imagenette dataset from Github to Imageneet folder
 """
 
+import logging as log
+import os
 from datetime import datetime
+
+import matplotlib.pyplot as plt
+import numpy as np
 import torch
 import torch.nn as nn
 import torchvision
 import torchvision.transforms as transforms
-import logging as log
-from models import resnet, alexnet, mycnn, mycnn2
-import os
-import matplotlib.pyplot as plt
 from matplotlib import ticker
-import numpy as np
-#from PIL import ImageFile
+
+from models import alexnet, mycnn, mycnn2, resnet
+
+# from PIL import ImageFile
 
 
 log.basicConfig(format="%(asctime)s %(message)s", level=log.INFO)
@@ -58,7 +61,7 @@ if modelname == "alexnet_":
     # Alexnet model works well for CIFAR-10 when input is scaled to 227x227
     model = alexnet.AlexNet().to(device)
     resize_to = transforms.Resize((227, 227))
-    
+
 if modelname == "RestNet50_":
     model = resnet.ResNet50(img_channel=3, num_classes=10).to(device)
     # resizing lower to keep it in memory
@@ -77,10 +80,13 @@ normalize_transform = transforms.Normalize(
 
 
 train_transforms = transforms.Compose(
-    [resize_to, 
-    transforms.RandomHorizontalFlip(),
-    transforms.RandomGrayscale(0.5),
-    transforms.ToTensor(), normalize_transform]
+    [
+        resize_to,
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomGrayscale(0.5),
+        transforms.ToTensor(),
+        normalize_transform,
+    ]
 )
 
 val_dir = os.path.join(data_dir, "val")
@@ -94,20 +100,22 @@ train_dataset = torchvision.datasets.ImageFolder(train_dir, train_transforms)
 val_dataset = torchvision.datasets.ImageFolder(val_dir, val_transforms)
 
 
-#-----------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------
 # Order the categories as per how Dataloader loads it
-#-----------------------------------------------------------------------------------------------------
+# -----------------------------------------------------------------------------------------------------
 
-foldername_to_class = { 'dogs50A-train' : "dog",
-                        'n01440764': "tench",
-                        'n02979186': "cassette player", 
-                        'n03000684': "chain saw",
-                        'n03028079': "church",
-                        'n03394916': "French horn",
-                        'n03417042': "garbage truck",
-                        'n03425413': "gas pump",
-                        'n03445777':  "golf ball",
-                        'n03888257': "parachute" }
+foldername_to_class = {
+    "dogs50A-train": "dog",
+    "n01440764": "tench",
+    "n02979186": "cassette player",
+    "n03000684": "chain saw",
+    "n03028079": "church",
+    "n03394916": "French horn",
+    "n03417042": "garbage truck",
+    "n03425413": "gas pump",
+    "n03445777": "golf ball",
+    "n03888257": "parachute",
+}
 
 # Imagenette classes - labels for better description
 categories_ref = [
@@ -124,15 +132,14 @@ categories_ref = [
 ]
 
 # sort as value to fit the directory order to labels to be sure
-print("Image to Folder Index",train_dataset.class_to_idx)
+print("Image to Folder Index", train_dataset.class_to_idx)
 sorted_vals = dict(sorted(train_dataset.class_to_idx.items(), key=lambda item: item[1]))
-categories =[]
+categories = []
 for key in sorted_vals:
     classname = foldername_to_class[key]
     categories.append(classname)
 
-log.info("Categories",categories)
-
+log.info("Categories", categories)
 
 
 # -------------------------------------------------------------------------------------------------------
@@ -147,12 +154,12 @@ torch.cuda.reset_max_memory_allocated()
 torch.cuda.synchronize()
 
 
-#ImageFile.LOAD_TRUNCATED_IMAGES = True # Use the data_checker.py and remove bad files instead of using this
+# ImageFile.LOAD_TRUNCATED_IMAGES = True # Use the data_checker.py and remove bad files instead of using this
 
 train_loader = torch.utils.data.DataLoader(
     train_dataset,
     batch_size=batch_size,
-    shuffle=True, #IMPORTANT otherwise the data is not shuffled
+    shuffle=True,  # IMPORTANT otherwise the data is not shuffled
     num_workers=workers,
     pin_memory=pin_memory,
     sampler=None,
@@ -190,7 +197,7 @@ if stepsize < 10:
 # loop over our epochs
 for epoch in range(0, num_epochs):
     # set the model in training mode
-    model.train() #IMPORTANT otherwise the model is not in training mode
+    model.train()  # IMPORTANT otherwise the model is not in training mode
     # initialize the total training and validation loss
     totalTrainLoss = 0
     totalValLoss = 0
@@ -205,20 +212,20 @@ for epoch in range(0, num_epochs):
 
         try:
             # Train in auto-mode with 16 bit mode
-            #with torch.autocast(device_type='cuda', dtype=torch.float16):
-            #Train in normal mode
-            with torch.autocast(device_type='cuda', dtype=torch.float32):
+            # with torch.autocast(device_type='cuda', dtype=torch.float16):
+            # Train in normal mode
+            with torch.autocast(device_type="cuda", dtype=torch.float32):
                 # send the input to the device
                 (images, labels) = (images.to(device), labels.to(device))
                 # perform a forward pass and calculate the training loss
                 outputs = model(images)
                 # output is float16 because linear layers autocast to float16.
-                #assert outputs.dtype is torch.float16 or 64
+                # assert outputs.dtype is torch.float16 or 64
 
                 loss = lossFn(outputs, labels)
                 # zero out the gradients, perform the backpropagation step,
                 # and update the weights
-                opt.zero_grad() #IMPORTANT otherwise the gradients of previous batches are not zeroed out
+                opt.zero_grad()  # IMPORTANT otherwise the gradients of previous batches are not zeroed out
         except Exception as e:
             log.error(f"Exception in data processing- skip and continue = {e}")
         loss.backward()
@@ -245,21 +252,19 @@ for epoch in range(0, num_epochs):
     )
 
 # Save the model
-path = "cnn/saved_models/" 
-model_save_name =path+ modelname + datetime.now().strftime("%H:%M_%B%d%Y")
-torch.save(
-    model.state_dict(),model_save_name + ".pth"
-)
+path = "cnn/saved_models/"
+model_save_name = path + modelname + datetime.now().strftime("%H:%M_%B%d%Y")
+torch.save(model.state_dict(), model_save_name + ".pth")
 log.info(f"Model {modelname} saved as {model_save_name}")
 
 # Generate the Confusion Matrix
 
-confusion_matrix = np.zeros((len(categories),len(categories)))
+confusion_matrix = np.zeros((len(categories), len(categories)))
 
 # In test phase, we don't need to compute gradients (for memory efficiency)
 with torch.no_grad():
-    model.eval() #IMPORTANT set model to eval mode before inference
-    
+    model.eval()  # IMPORTANT set model to eval mode before inference
+
     for images, labels in test_loader:
         images = images.to(device)
         labels = labels.to(device)
@@ -267,73 +272,93 @@ with torch.no_grad():
         # ------------------------------------------------------------------------------------------
         # Predict for the batch of images
         # ------------------------------------------------------------------------------------------
-        outputs = model(images)  #Outputs= torch.Size([64, 10]) Probability of each of the 10 classes
-        _, predicted = torch.max(outputs.data, 1) # get the class with the highest Probability out Given 1 per image # predicted= torch.Size([64])
+        outputs = model(
+            images
+        )  # Outputs= torch.Size([64, 10]) Probability of each of the 10 classes
+        _, predicted = torch.max(
+            outputs.data, 1
+        )  # get the class with the highest Probability out Given 1 per image # predicted= torch.Size([64])
         # ------------------------------------------------------------------------------------------
         #  Lets check also which classes are wrongly predicted with other classes  to create a MultiClass confusion matrix
         # ------------------------------------------------------------------------------------------
 
-        mask=(predicted != labels) # Wrongly predicted
-        wrong_predicted =torch.masked_select(predicted,mask)
-        wrong_labels =torch.masked_select(labels,mask)
-        wrongly_zipped = zip(wrong_labels,wrong_predicted)
+        mask = predicted != labels  # Wrongly predicted
+        wrong_predicted = torch.masked_select(predicted, mask)
+        wrong_labels = torch.masked_select(labels, mask)
+        wrongly_zipped = zip(wrong_labels, wrong_predicted)
 
-        mask=(predicted == labels) # Rightly predicted
-        rightly_predicted =torch.masked_select(predicted,mask)
-        right_labels =rightly_predicted #same torch.masked_select(labels,mask)
-        rightly_zipped = zip(right_labels,rightly_predicted)
-        
+        mask = predicted == labels  # Rightly predicted
+        rightly_predicted = torch.masked_select(predicted, mask)
+        right_labels = rightly_predicted  # same torch.masked_select(labels,mask)
+        rightly_zipped = zip(right_labels, rightly_predicted)
+
         # Note that this is for a single batch - add to the list associated with class
-        for _,j in enumerate(wrongly_zipped):
-            k = j[0].item() # label
-            l = j[1].item() # predicted
-            confusion_matrix[k][l] +=1
-       
+        for _, j in enumerate(wrongly_zipped):
+            k = j[0].item()  # label
+            l = j[1].item()  # predicted
+            confusion_matrix[k][l] += 1
+
         # Note that this is for a single batch - add to the list associated with class
-        for _,j in enumerate(rightly_zipped):
-            k = j[0].item() # label
-            l = j[1].item() # predicted
-            confusion_matrix[k][l] +=1
-    
-    #print("Confusion Matrix1=\n",confusion_matrix)
+        for _, j in enumerate(rightly_zipped):
+            k = j[0].item()  # label
+            l = j[1].item()  # predicted
+            confusion_matrix[k][l] += 1
+
+    # print("Confusion Matrix1=\n",confusion_matrix)
     # ------------------------------------------------------------------------------------------
     # Print Confusion matrix in Pretty print format
     # ------------------------------------------------------------------------------------------
     print(categories)
     for i in range(len(categories)):
         for j in range(len(categories)):
-            print(f"\t{confusion_matrix[i][j]}",end='')
-        print(f"\t{categories[i]}\n",end='')
+            print(f"\t{confusion_matrix[i][j]}", end="")
+        print(f"\t{categories[i]}\n", end="")
     # ------------------------------------------------------------------------------------------
     # Calculate Accuracy per class
     # ------------------------------------------------------------------------------------------
     print("---------------------------------------")
-    print(f"Accuracy/precision from confusion matrix is {round(confusion_matrix.trace()/confusion_matrix.sum(),2)}")
+    print(
+        f"Accuracy/precision from confusion matrix is {round(confusion_matrix.trace()/confusion_matrix.sum(),2)}"
+    )
     print("---------------------------------------")
     for i in range(len(categories)):
-        print(f"---Accuracy for class {categories[i]} = {round(confusion_matrix[i][i]/confusion_matrix[i].sum(),2)}")
-    
+        print(
+            f"---Accuracy for class {categories[i]} = {round(confusion_matrix[i][i]/confusion_matrix[i].sum(),2)}"
+        )
+
     # ---------------------------------------------------
     # Plot this in a good figure
     # ---------------------------------------------------
-        
 
     fig = plt.figure()
     ax = fig.add_subplot(111)
-    plt.title('Confusion Matrix', fontsize=18)
+    plt.title("Confusion Matrix", fontsize=18)
     ax.matshow(confusion_matrix, cmap=plt.cm.Blues, alpha=0.7)
-    ax.set_xticklabels([''] + categories,rotation=90)
-    ax.set_yticklabels([''] + categories)
+    ax.set_xticklabels([""] + categories, rotation=90)
+    ax.set_yticklabels([""] + categories)
     ax.xaxis.set_major_locator(ticker.MultipleLocator(1))
     ax.yaxis.set_major_locator(ticker.MultipleLocator(1))
     for i in range(confusion_matrix.shape[0]):
         for j in range(confusion_matrix.shape[1]):
-            ax.text(x=j, y=i,s=int(confusion_matrix[i, j]), va='center', ha='center', size='xx-small')
-            if ( i==j):
-                acc = round(confusion_matrix[i][i]/confusion_matrix[i].sum(),2)
-                ax.text(x=len(categories)+1, y=i,s=acc, va='center', ha='center', size='xx-small')
-    plt.savefig(model_save_name +"_cm.jpg")
-
+            ax.text(
+                x=j,
+                y=i,
+                s=int(confusion_matrix[i, j]),
+                va="center",
+                ha="center",
+                size="xx-small",
+            )
+            if i == j:
+                acc = round(confusion_matrix[i][i] / confusion_matrix[i].sum(), 2)
+                ax.text(
+                    x=len(categories) + 1,
+                    y=i,
+                    s=acc,
+                    va="center",
+                    ha="center",
+                    size="xx-small",
+                )
+    plt.savefig(model_save_name + "_cm.jpg")
 
     # correct = 0
     # total = 0
@@ -350,5 +375,3 @@ with torch.no_grad():
     #         total, 100 * correct / total
     #     )
     # )
-
-
